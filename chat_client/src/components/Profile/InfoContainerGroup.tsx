6008c9d1983f940a9eb6954a71cc4b9a7400947c
RudovichNazar23 <n.rudovich2005@gmail.com>
2025-03-27 09:50:24 +0100

    added basic layout to user profile component and made it responsive

--- chat_client/src/components/Profile/InfoContainerGroup.tsx
+++ chat_client/src/components/Profile/InfoContainerGroup.tsx
@@ -9,10 +9,10 @@ interface InfoContainerGroupProps {
 
 export default function InfoContainerGroup({ labelValue, textValue, isEdit, inputType }: InfoContainerGroupProps): JSX.Element {
     return (
-        <div className="col-span-8">
-            <div className="grid grid-cols-8 p-5 gap-0.5">
-                <div className={"col-span-1"}>{ labelValue }:</div>
-                <div className={"col-span-7 text-start"}>{ textValue }</div>
+        <div className="md:col-span-8 col-span-12">
+            <div className="grid md:grid-cols-8 grid-cols-12 md:p-5 p-2 md:gap-0.5">
+                <div className={"md:col-span-1 col-span-6"}>{ labelValue }:</div>
+                <div className={"md:col-span-7 text-start col-span-6"}>{ textValue }</div>
             </div>
         </div>
     );
